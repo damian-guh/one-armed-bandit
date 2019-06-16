ed2059ea8c58c2ef7b38be27859c6fe5cb6b73b2
ThisGuh <jakiepolska1111@gmail.com>
2019-06-16 18:14:11 +0200

    statistics class

---
+++ Statistics.js
@@ -0,0 +1,21 @@
+class Statistics {
+    constructor() {
+        this.gameResults = [{
+            win,
+            bid
+        }];
+    }
+    addGameToStatistics(win, bid) {
+        let gameResult = {
+            win,
+            bid
+        }
+        this.gameResults.push(gameResult);
+    }
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(result => result.win).length;
+        let losses = this.gameResults.filter(result => !result.win).length;
+        return [games, wins, losses];
+    }
+}
