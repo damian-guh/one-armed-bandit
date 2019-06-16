ea448fd21eed7cf23225d6196be8bb33b4a633b6
ThisGuh <jakiepolska1111@gmail.com>
2019-06-16 14:18:21 +0200

    Wallet class

---
+++ Wallet.js
@@ -0,0 +1,27 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+        this.getWalletValue = () => _money;
+        this.checkCanPlay = (value) => {
+            if (_money >= value) {
+                return true;
+            }
+            return false;
+        }
+        this.changeWallet = (value, type = "+") => {
+            if (typeof value === "number" && !isNaN(value)) {
+                if (type === "+") {
+                    return _money += value;
+                } else if (type === "-") {
+                    return _money -= value;
+                } else {
+                    throw new Error("incorrect type");
+                }
+            } else {
+                throw new Error("incorrect number");
+            }
+        }
+
+    }
+}
+const wallet = new Wallet(100);
